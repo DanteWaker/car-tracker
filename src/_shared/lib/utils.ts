f2922b38e38e8ffd9e1f7eb1ff01d08d28e7601e
Denner <denner.nascimento@protonmail.com>
2025-03-17 04:10:26 -0300

    feat: add JWT decoding utility function to enhance token handling

--- src/_shared/lib/utils.ts
+++ src/_shared/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function decodeJwt(token: string) {
+	const decoded = JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+	return decoded;
+}
